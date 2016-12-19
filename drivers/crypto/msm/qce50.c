381fd6de306e5a7c4cd7c0835917cffbdb14e8e2
AnilKumar Chimata <anilc@codeaurora.org>
2016-12-19 18:12:06 +0530

    msm: crypto: set CLR_CNTXT bit for crypto operations
    HLOS Crypto driver needs to set CLR_CNTXT bit for operations with
    legacy software key registers.
    
    Change-Id: Iff482f726d106e99a4006f7077a171da3c7ca9c3
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: AnilKumar Chimata <anilc@codeaurora.org>

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -1813,8 +1813,7 @@ static int _ce_f9_setup_direct(struct qce_device *pce_dev,
 							CRYPTO_CONFIG_REG));
 	/* write go */
 	writel_relaxed(((1 << CRYPTO_GO) | (1 << CRYPTO_RESULTS_DUMP) |
-				(1 << CRYPTO_CLR_CNTXT)),
-				pce_dev->iobase +  CRYPTO_GOPROC_REG);
+		(1 << CRYPTO_CLR_CNTXT)), pce_dev->iobase +  CRYPTO_GOPROC_REG);
 	/*
 	 * Ensure previous instructions (setting the GO register)
 	 * was completed before issuing a DMA transfer request
@@ -1891,8 +1890,7 @@ static int _ce_f8_setup_direct(struct qce_device *pce_dev,
 							CRYPTO_CONFIG_REG));
 	/* write go */
 	writel_relaxed(((1 << CRYPTO_GO) | (1 << CRYPTO_RESULTS_DUMP) |
-				(1 << CRYPTO_CLR_CNTXT)),
-				pce_dev->iobase +  CRYPTO_GOPROC_REG);
+		(1 << CRYPTO_CLR_CNTXT)), pce_dev->iobase +  CRYPTO_GOPROC_REG);
 	/*
 	 * Ensure previous instructions (setting the GO register)
 	 * was completed before issuing a DMA transfer request
