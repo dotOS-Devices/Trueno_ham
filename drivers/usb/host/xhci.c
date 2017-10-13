e6dd1407f3a19f0a70541f1cc86e8845d2214951
Srijith <srijithdamerla2001@gmail.com>
2017-10-13 15:45:52 +0530

    xhci: Remove redeclaration

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -792,22 +792,6 @@ void xhci_shutdown(struct usb_hcd *hcd)
 
 #ifdef CONFIG_PM
 
-#ifdef CONFIG_PCI
-static void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
-{
-	int i;
-
-	if (xhci->msix_entries) {
-		for (i = 0; i < xhci->msix_count; i++)
-			synchronize_irq(xhci->msix_entries[i].vector);
-	}
-}
-#else
-static void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
-{
-}
-#endif /* CONFIG_PCI */
-
 static void xhci_save_registers(struct xhci_hcd *xhci)
 {
 	xhci->s3.command = xhci_readl(xhci, &xhci->op_regs->command);
