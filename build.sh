bf3a5bdee4afeb53c199b8bed67ff309da528d64
Ram9199 <sriramscience7@gmail.com>
2018-06-26 04:13:47 +0000

    dtb: Add dtb target

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-### Srijith Bharadwaj (srijithdamerla2001@gmail.com)
-### A custom build script to build zImage
+### Prema Chand Alugu (premaca@gmail.com)
+### Shivam Desai (shivamdesaixda@gmail.com)
+### Martichou (martichou.andre@gmail.com)
+### A custom build script to build zImage & DTB(Anykernel2 method)
 set -e
 ## Copy this script inside the kernel directory
 
 KERNEL_DIR=$PWD
 KERNEL_TOOLCHAIN=$KERNEL_DIR/toolchain-4.9/bin/arm-eabi-
 KERNEL_DEFCONFIG=StarCity_defconfig
+DTBTOOL=$KERNEL_DIR/tools/dtbtool
 JOBS=8
+FINAL_KERNEL_ZIP=StarCity_ham.zip
+DTBIMAGE="dtb"
 
 # The MAIN Part
 echo "**** Setting Toolchain ****"
@@ -22,5 +27,12 @@ echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
 make $KERNEL_DEFCONFIG
 make -j$JOBS
 
-echo "**** Build is completed ****"
-echo "Get zImage from arch/arm/boot"
+# Time for dtb
+echo "**** Generating DT.IMG ****"
+$DTBTOOL/dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm/boot/
+
+echo "**** Verify zImage & dtb ****"
+ls $KERNEL_DIR/arch/arm/boot/zImage
+ls $KERNEL_DIR/arch/arm/boot/$DTBIMAGE
+
+echo "**** Build Successful ****"
